d8f8a05bd2e32d09b05a6c4b6ee2f4b0a2fe374c
AnGabSS <angelo.gs.santana@outlook.com>
2025-07-02 16:51:26 -0300

    [FIX] remove SpringBootTest annotation because initially only the business logic will be tested

--- src/test/java/com/tech/padawan/financialmanager/FinancialmanagerApplicationTests.java
+++ src/test/java/com/tech/padawan/financialmanager/FinancialmanagerApplicationTests.java
@@ -1,9 +1,16 @@
 package com.tech.padawan.financialmanager;
 
+import com.tech.padawan.financialmanager.global.config.security.SecurityConfiguration;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FinancialmanagerApplicationTests {
 
 	@Test
